7092503cc4bd2c8d65fdb22ac0f2f59ff78ddfe1
Charles-Andre Bouffard <cabouffard@gmail.com>
2016-08-10 15:46:46 -0400

    Fix rubocop offense

--- db/migrate/20160809174314_add_pos_order_id_to_orders.rb
+++ db/migrate/20160809174314_add_pos_order_id_to_orders.rb
@@ -1,4 +1,4 @@
-class AddPosOrderIdToOrders< ActiveRecord::Migration
+class AddPosOrderIdToOrders < ActiveRecord::Migration
   def change
     add_column :spree_orders, :pos_order_id, :string
     add_index :spree_orders, :pos_order_id
