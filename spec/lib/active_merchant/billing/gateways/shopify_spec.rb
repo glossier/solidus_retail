e645eb62b6c2bb47c97bc617cae58ceaa6db3f6a
Daniel Wright <daniel@godynamo.com>
2016-08-26 13:52:58 -0400

    First Attempt at Porting Shopify AM Gateway Unit Tests
    There's plenty of opportunity for improvement here. I'm not sure how much I
    will contribute, though, as I'm not sure this is a unit test. To be discussed.

---
+++ spec/lib/active_merchant/billing/gateways/shopify_spec.rb
@@ -0,0 +1,107 @@
+require 'spec_helper'
+require 'active_merchant/billing/gateways/shopify'
+
+module ActiveMerchant::Billing
+  RSpec.describe ShopifyGateway do
+    let(:api_key) { 'api_key' }
+    let(:password) { 'password' }
+    let(:shop_name) { 'shop_name' }
+
+    let(:gateway) do
+      described_class.new(api_key: api_key,
+                          password: password,
+                          shop_name: shop_name)
+    end
+
+    around(:each) do |example|
+      begin
+        previously = ShopifyAPI::Base.site
+        example.run
+      ensure
+        ShopifyAPI::Base.site = previously
+      end
+    end
+
+    describe "#void" do
+      let(:transaction_id) { '123' }
+      let(:transaction_amount) { 1 }
+      let(:transaction) { double(:transaction, amount: transaction_amount, id: transaction_id) }
+
+      let(:refunder) { double(:refunder, perform: true) }
+
+      before do
+        allow(ShopifyAPI::Transaction).to receive(:find).and_return(transaction)
+        allow(ShopifyRefunder).to receive(:new).and_return(refunder)
+      end
+
+      it "performs a refund" do
+        expect(refunder).to receive(:perform).once
+
+        gateway.void(transaction_id, { order_id: transaction_id })
+      end
+
+      context "when the transaction can't be found" do
+        let(:transaction) { nil }
+
+        it "raises an exception" do
+          cause = ->{ gateway.void(transaction_id, { order_id: transaction_id }) }
+
+          expect(&cause).to raise_error ::ActiveMerchant::Billing::ShopifyGateway::TransactionNotFoundError
+        end
+      end
+    end
+
+    describe "#refund" do
+      let(:refund_amount) { 100 }
+      let(:refund_errors) { [] }
+      let(:refund_reason) { 'reason' }
+      let(:transaction_id) { '123' }
+      let(:transaction_amount) { 1 }
+
+      let(:refund) { double(:refund, errors: refund_errors) }
+      let(:transaction) { double(:transaction, amount: transaction_amount, id: transaction_id) }
+
+      before do
+        allow(ShopifyAPI::Transaction).to receive(:find).and_return(transaction)
+        allow(ShopifyAPI::Refund).to receive(:create).and_return(refund)
+      end
+
+      it "refunds the customer successfully" do
+        cause = ->{ gateway.refund(refund_amount, transaction_id, { order_id: transaction_id, reason: refund_reason }) }
+
+        expect(cause.call).to be_success
+      end
+
+      context "when the transaction can't be found" do
+        let(:refund_amount) { 100 }
+        let(:transaction) { nil }
+
+        it "raises an exception" do
+          cause = ->{ gateway.refund(refund_amount, transaction_id, { order_id: transaction_id, reason: refund_reason }) }
+
+          expect(&cause).to raise_error ::ActiveMerchant::Billing::ShopifyGateway::TransactionNotFoundError
+        end
+      end
+
+      context "when the amount to be credited exceeds the original transaction" do
+        let(:refund_amount) { 10_000 }
+
+        it "raises an exception" do
+          cause = ->{ gateway.refund(refund_amount, transaction_id, { order_id: transaction_id, reason: refund_reason }) }
+
+          expect(&cause).to raise_error ::ActiveMerchant::Billing::ShopifyGateway::CreditedAmountBiggerThanTransaction
+        end
+      end
+
+      context "when a validation error is encountered" do
+        let(:refund_errors) { double(:errors, messages: { error: 'error1' }) }
+
+        it "fails to perform the refund" do
+          cause = ->{ gateway.refund(refund_amount, transaction_id, { order_id: transaction_id, reason: refund_reason }) }
+
+          expect(cause.call).to_not be_success
+        end
+      end
+    end
+  end
+end
