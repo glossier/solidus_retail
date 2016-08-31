88c8da47eac312c3d41d25817115d9938d88a6e0
Charles-Andre Bouffard <cabouffard@gmail.com>
2016-08-31 16:57:03 -0400

    Add spec for the refund decorator

--- app/model/spree/refund_decorator.rb
+++ app/model/spree/refund_decorator.rb
@@ -1,6 +1,10 @@
-Spree::Refund.class_eval do
-  def pos_order_id
-    # NOTE(cab): Should we use delegates here?
-    payment.order.pos_order_id
+module Spree
+  module RefundDecorator
+    def pos_order_id
+      # NOTE(cab): Should we use delegates here?
+      payment.order.pos_order_id
+    end
   end
 end
+
+Spree::Refund.prepend Spree::RefundDecorator
