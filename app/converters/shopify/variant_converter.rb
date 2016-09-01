f5e5de928ea3b11d65c0ad0f14d98e3d72ed028f
Charles-Andre Bouffard <cabouffard@gmail.com>
2016-09-01 14:08:57 -0400

    Add default value to the type of weight a variant has

--- app/converters/shopify/variant_converter.rb
+++ app/converters/shopify/variant_converter.rb
@@ -1,13 +1,14 @@
 module Shopify
   class VariantConverter
-    def initialize(spree_variant, shopify_variant)
+    def initialize(spree_variant, shopify_variant, weight_unit = nil)
       @spree_variant = spree_variant
       @shopify_variant = shopify_variant
+      @weight_unit = weight_unit || default_weight_unit
     end
 
     def perform
       shopify_variant.weight = spree_variant.weight
-      shopify_variant.weight_unit = 'oz'
+      shopify_variant.weight_unit = weight_unit
       shopify_variant.price = spree_variant.price
       shopify_variant.sku = spree_variant.sku
       shopify_variant.updated_at = spree_variant.updated_at
@@ -18,7 +19,7 @@ module Shopify
 
     private
 
-    attr_accessor :spree_variant, :shopify_variant
+    attr_accessor :spree_variant, :shopify_variant, :weight_unit
 
     def generate_options!
       assign_variant_uniqueness_constraint(spree_variant.sku)
@@ -30,6 +31,11 @@ module Shopify
 
     def assign_variant_uniqueness_constraint(value)
       shopify_variant.option1 = value
+    end
+
+    # NOTE: The weight_unit can be either 'g', 'kg, 'oz', or 'lb'.
+    def default_weight_unit
+      'oz'
     end
   end
 end
