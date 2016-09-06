808f00c53da16c4936383bef9e723a7c8943c002
Daniel Wright <daniel@godynamo.com>
2016-09-06 11:39:15 -0400

    Cleans up ShopifyRefunder Spec
    - Consolidates `subject` declarations
    - Moves MockError definition closer to invokation for clarity
    - Other minor readability tweaks

--- spec/services/spree/retail/shopify_refunder_spec.rb
+++ spec/services/spree/retail/shopify_refunder_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 module Spree::Retail
   RSpec.describe ShopifyRefunder do
-    class MockError < RuntimeError; end
-
     let(:transaction_id) { '0xDEADBEEF' }
     let(:options) { { reason: 'Actual reason', order_id: transaction_id } }
     let(:transaction_amount) { 1 }
@@ -11,28 +9,28 @@ module Spree::Retail
     let(:transaction_instance) { double('transaction_instance', amount: transaction_amount, id: transaction_id) }
     let(:transaction_interface) { double('transaction_interface', find: transaction_instance) }
 
-    context '.initialize' do
-      subject { described_class.new(credited_money_in_cents, transaction_id, options, transaction_interface) }
+    let(:can_issue_refund_policy_klass) { double(:can_issue_refund_policy_klass, new: can_issue_refund_policy) }
+    let(:can_issue_refund_policy) { double(:can_issue_refund_policy) }
 
+    let(:pos_refund) { double('refund') }
+    let(:refunder_interface) { double('refunder_interface', create: pos_refund) }
+
+    subject(:refunder) do
+      described_class.new(credited_money_in_cents,
+                          transaction_id,
+                          options,
+                          transaction_interface,
+                          refunder_interface,
+                          can_issue_refund_policy_klass)
+    end
+
+    context '.initialize' do
       it 'successfully does its thing' do
-        expect(subject).to be_truthy
+        expect(refunder).to be_a described_class
       end
     end
 
     context '.perform' do
-      let(:can_issue_refund_policy_klass) { double(:can_issue_refund_policy_klass) }
-      let(:can_issue_refund_policy) { double(:can_issue_refund_policy) }
-      let(:pos_refund) { double('refund') }
-      let(:refunder_interface) { double('refunder_interface', create: pos_refund) }
-
-      subject do
-        described_class.new(credited_money_in_cents, transaction_id, options, transaction_interface, refunder_interface, can_issue_refund_policy_klass)
-      end
-
-      before do
-        allow(can_issue_refund_policy_klass).to receive(:new).and_return(can_issue_refund_policy)
-      end
-
       context "when the refund can be issued" do
         before do
           allow(can_issue_refund_policy).to receive(:allowed?).and_return(true)
@@ -41,11 +39,13 @@ module Spree::Retail
         it 'performs a refund in shopify' do
           expect(refunder_interface).to receive(:create)
 
-          subject.perform
+          refunder.perform
         end
       end
 
       context 'when the refund cannot be issued' do
+        class MockError < RuntimeError; end
+
         let(:mock_error) { MockError.new }
 
         before do
@@ -53,7 +53,7 @@ module Spree::Retail
         end
 
         it 'raises an error' do
-          cause = ->{ subject.perform }
+          cause = ->{ refunder.perform }
           expect(&cause).to raise_error MockError
         end
       end
