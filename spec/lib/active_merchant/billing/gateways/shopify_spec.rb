222186acd2d4a2cc25cce6d19d017c478d0b750f
Daniel Wright <daniel@godynamo.com>
2016-09-07 17:29:11 -0400

    ActiveMerchant::Billing::ShopifyGateway Spec Cleanup
    - consolidates instantiation arguments and injected dependencies;
    - makes various syntax and usage a little more consistent;
    - adds a missing expectation in the `#refund` specs;
    - removes a superfluous `#refund` spec.

--- spec/lib/active_merchant/billing/gateways/shopify_spec.rb
+++ spec/lib/active_merchant/billing/gateways/shopify_spec.rb
@@ -3,78 +3,81 @@ require 'active_merchant/billing/gateways/shopify'
 
 module ActiveMerchant::Billing
   RSpec.describe ShopifyGateway do
+    # Arguments
     let(:api_key) { 'api_key' }
     let(:password) { 'password' }
     let(:shop_name) { 'shop_name' }
-    let(:transaction_id) { '0xDEADBEEF' }
-    let(:pos_refund) { double('refund', errors: []) }
+
+    # Injected dependencies
+    let(:refunder_factory) { double(:refunder_factory, new: refunder) }
+    let(:refunder) { double(:refunder, perform: pos_refund) }
+    let(:transaction_repository) { double(:transaction_repository, find: transaction) }
+    let(:transaction) { double(:transaction, id: transaction_id, amount: 100) }
+
+    let(:gateway) do
+      described_class.new(api_key: api_key,
+                          password: password,
+                          shop_name: shop_name,
+                          refunder: refunder_factory,
+                          transaction_repository: transaction_repository)
+    end
+
+    let(:pos_refund) { double('refund', errors: pos_refund_errors) }
+    let(:pos_refund_errors) { [] }
 
     describe '#refund' do
-      let(:refund_amount) { 100 }
-      let(:refunder_instance) { double('refunder_instance', perform: pos_refund) }
-      let(:refunder) { double('refunder', new: refunder_instance) }
+      let(:order_id) { "0xCAFED00D" }
+      let(:refund_amount) { 200 }
+      let(:transaction_id) { '0xDEADBEEF' }
 
-      subject { described_class.new(api_key: api_key, password: password, shop_name: shop_name, refunder: refunder) }
-
-      it 'performs a refund' do
-        expect(refunder_instance).to receive(:perform).once
-        refund!
+      subject(:refund!) do
+        gateway.refund(refund_amount, transaction_id, { order_id: order_id })
       end
 
-      it 'returns an ActiveMerchant response' do
-        result = refund!
-        expect(result).to be_a(ActiveMerchant::Billing::Response)
+      before do
+        allow(refunder).to receive(:perform).and_return(pos_refund)
+      end
+
+      it 'performs a refund' do
+        expect(refunder_factory).to receive(:new).with(credited_money: 200,
+                                                       transaction_id: '0xDEADBEEF',
+                                                       order_id: '0xCAFED00D')
+        expect(refunder).to receive(:perform).once
+
+        refund!
       end
 
       context 'when refund was successful' do
         it 'returns an ActiveMerchant successful response' do
           result = refund!
+
           expect(result).to be_success
         end
       end
 
       context 'when refund was not successful' do
-        let(:pos_refund) { double('refund', errors: refund_errors) }
-        let(:refund_errors) { double('errors', messages: ['I am ERROR']) }
-
-        before do
-          allow(refunder_instance).to receive(:perform).and_return(pos_refund)
-        end
+        let(:pos_refund_errors) { double('errors', messages: ['I am ERROR']) }
 
         it 'returns an ActiveMerchant unsuccessful response' do
           result = refund!
+
           expect(result).not_to be_success
         end
-      end
-
-      private
-
-      def refund!
-        subject.refund(refund_amount, transaction_id, { order_id: transaction_id })
       end
     end
 
     describe '#void' do
+      let(:transaction_id) { "0xDEADBEEF" }
       let(:order_id) { "0xCAFED00D" }
-      let(:refunder_factory) { double(:refunder_factory, new: refunder) }
-      let(:refunder) { double(:refunder, perform: pos_refund) }
-
-      let(:transaction_repository) { double(:transaction_repository, find: transaction) }
-
-      let(:gateway) do
-        described_class.new(api_key: api_key, password: password, shop_name: shop_name, refunder: refunder_factory, transaction_repository: transaction_repository)
-      end
 
       subject(:void!) do
         gateway.void(transaction_id, { order_id: order_id })
       end
 
-      let(:transaction) { double(:transaction, id: 1234, amount: 100) }
-
       it 'refunds the transaction' do
         expect(refunder_factory).to receive(:new).with(credited_money: 100,
-                                                       transaction_id: 1234,
-                                                       order_id: "0xCAFED00D")
+                                                       transaction_id: '0xDEADBEEF',
+                                                       order_id: '0xCAFED00D')
         expect(refunder).to receive(:perform)
 
         void!
@@ -89,8 +92,7 @@ module ActiveMerchant::Billing
       end
 
       context 'when refund was not successful' do
-        let(:pos_refund) { double('refund', errors: refund_errors) }
-        let(:refund_errors) { double('errors', messages: ['I am ERROR']) }
+        let(:pos_refund_errors) { double(:errors, messages: ['I am ERROR']) }
 
         it 'returns an ActiveMerchant unsuccessful response' do
           result = void!
