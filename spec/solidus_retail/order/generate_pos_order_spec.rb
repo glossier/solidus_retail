cf16d54844ad6e676ed269afcb55af0369204720
Braden <braden.douglass@gmail.com>
2016-09-14 09:58:27 -0400

    Add In More Requirements For Testing Order Import
    Require the shopify_api in the lib test along with adding in the shared
    context that allows mocking of requests to the Shopify API.
    
    Clubhouse:
    https://app.clubhouse.io/dynamomtl/story/48/

--- spec/solidus_retail/order/generate_pos_order_spec.rb
+++ spec/solidus_retail/order/generate_pos_order_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 require 'solidus_retail/order/generate_pos_order'
+require 'shopify_api'
 
 RSpec.describe SolidusRetail::Order::GeneratePosOrder, type: :model do
+  include_context 'shopify_shop'
+
+  it 'instantiates itself successfully' do
+  end
 end
