b864d3d5cc94227ebd7d9ae10a202ea1ca0244ec
Daniel Wright <daniel@godynamo.com>
2016-09-16 10:58:10 -0400

    VCR Configuration and Cassettes for Request Specs
    The configuration and cassettes below should allow Travis to use recorded
    responses for its builds, relaxing the requirement for real Shopify credentials
    to be used.
    
    Recorded responses must be recorded outside the Travis environment. The
    implication is Travis will _not_ catch broken builds caused by Shopify API
    changes. Whether this trade-off is worthwhile is up for discussion.

---
+++ spec/support/vcr.rb
@@ -0,0 +1,15 @@
+require 'vcr'
+require 'webmock'
+
+VCR.configure do |c|
+  c.allow_http_connections_when_no_cassette = false
+  c.cassette_library_dir = 'spec/cassettes'
+  c.configure_rspec_metadata!
+  c.hook_into :webmock
+  c.ignore_localhost = true
+
+  c.default_cassette_options = {
+    record: ENV.key?('TRAVIS') ? :none : :new_episodes,
+    match_requests_on: [:method, :path]
+  }
+end
