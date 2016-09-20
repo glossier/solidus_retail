ee70fcb60335a5eb4a5b2bfffb27b232517ed840
Charles-Andre Bouffard <cabouffard@gmail.com>
2016-09-20 16:10:06 -0400

    Add the multiple folders to cover
    This will let us have better test coverage percentage. Prior to that
    change multiple folders were not in the list of folders to check for
    coverage which would generally give us a test coverage of 95%>, which is
    not what we currently have.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,15 @@ SimpleCov.start do
   add_group 'Mailers', 'app/mailers'
   add_group 'Models', 'app/models'
   add_group 'Views', 'app/views'
+  add_group 'Delegators', 'app/delegators'
+  add_group 'Policies', 'app/policies'
+  add_group 'Renderers', 'app/renderers'
+  add_group 'Converters', 'app/converters'
+  add_group 'Exporters', 'app/exporters'
+  add_group 'Jobs', 'app/jobs'
+  add_group 'Permuters', 'app/permuters'
+  add_group 'Presenters', 'app/presenters'
+  add_group 'Services', 'app/services'
   add_group 'Libraries', 'lib'
 end
 
