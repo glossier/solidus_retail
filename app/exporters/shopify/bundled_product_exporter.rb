d7be88a79df9da42cda4ae558736acdcc804f84a
Charles-Andre Bouffard <cabouffard@gmail.com>
2016-09-21 14:28:27 -0400

    Change the find_by methods
    I have detected a problem with the base_decorator where it would not
    find the proper variant. I was always testing with a Shopify Shop that
    was empty and now that we have some real data a problem came out.
    
    We were always returning all the variants even though we were trying to
    find one with a specific id and we first picking the first one from that
    array.
    
    Instead, I've decided to use the `find` method that they are already
    using in their ShopifyAPI gem and catching the 404 error. When that
    exception is caught we create a new instance of that object and return
    it.
    
    This also regenerate the cassettes, due to the fact that the
    base_decorator has changed which indirectly make the requests change.

--- app/exporters/shopify/bundled_product_exporter.rb
+++ app/exporters/shopify/bundled_product_exporter.rb
@@ -24,7 +24,7 @@ module Shopify
       :bundle_attributes
 
     def find_shopify_product_for(spree_product)
-      product_api.find_or_initialize_by(id: spree_product.pos_product_id)
+      product_api.find_or_initialize_by_id(spree_product.pos_product_id)
     end
 
     def save_associations_for(spree_product, shopify_product)
