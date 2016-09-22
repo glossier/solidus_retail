b676704e8ab53d5c5e5207fa8382954529f840ed
Charles-Andre Bouffard <cabouffard@gmail.com>
2016-09-22 13:32:42 -0400

    Add specs for the variant presenter class
    This will verify that those methods exists and correctly returns what we
    want.
    
    We have to keep in mind that those methods are made to be overwritten by
    the application that will use this engine.
    
    Resolves https://github.com/glossier/solidus_retail/issues/22

---
+++ spec/presenters/shopify/variant_presenter_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+module Shopify
+  RSpec.describe VariantPresenter do
+    include Spree::Retail::PresenterHelper
+    include_context 'spree_builders'
+
+    describe '.weight_unit' do
+      class DummyClass < ActiveResource::Base; end
+      subject { present(DummyClass.new, :variant) }
+
+      it 'returns oz as the default unit' do
+        result = subject.weight_unit
+        expect(result).to eql('oz')
+      end
+    end
+
+    describe '.default_pos_image' do
+      let(:variant) { build(:variant) }
+      let(:image) { build(:image) }
+
+      before do
+        variant.images << image
+      end
+
+      subject { present(variant, :variant) }
+
+      it 'returns the first image of the variant' do
+        result = subject.default_pos_image
+        expect(result).to eql(variant.images.first)
+      end
+    end
+  end
+end
