36c239d2b09bb4b3ec35019c3081f2ef48a5203b
Charles-Andre Bouffard <cabouffard@gmail.com>
2016-09-27 16:22:02 -0400

    Create the initializer when installing the gem
    Instead of having to run an extra-command, this file is REQUIRED for the
    gem to work, so I've decided to add it directly in the install
    generator.

--- lib/generators/spree/retail/install/install_generator.rb
+++ lib/generators/spree/retail/install/install_generator.rb
@@ -17,6 +17,10 @@ module SolidusRetail
         run 'bundle exec rake railties:install:migrations FROM=solidus_retail'
       end
 
+      def add_initializer
+        template 'config/initializers/spree_retail.rb', 'config/initializers/spree_retail.rb'
+      end
+
       def run_migrations
         run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]'))
         if run_migrations
