0d731f19c7080932e141c8547becf9a0500ad247
Charles-Andre Bouffard <cabouffard@gmail.com>
2016-09-28 10:58:21 -0400

    Change the namespace of the Updater classes

---
+++ app/updaters/spree/retail/shopify/product_updater.rb
@@ -0,0 +1,49 @@
+module Spree
+  module Retail
+    module Shopify
+      class ProductUpdater
+        def initialize(spree_product_id:, product_klass: Spree::Product,
+                       product_api: ShopifyAPI::Product,
+                       attributor: Shopify::ProductAttributes,
+                       exporter: Shopify::ProductExporter)
+
+          @spree_product = product_klass.find(spree_product_id)
+          @product_api = product_api
+          @exporter = exporter
+          @attributor = attributor
+        end
+
+        def perform
+          shopify_product = find_shopify_product_for(spree_product)
+
+          return export_product unless shopify_product.persisted?
+
+          update_shopify_product(shopify_product)
+        end
+
+        private
+
+        attr_accessor :spree_product, :product_api, :exporter, :attributor
+
+        def find_shopify_product_for(spree_product)
+          product_api.find_or_initialize_by_id(spree_product.pos_product_id)
+        end
+
+        def update_shopify_product(shopify_product)
+          shopify_product.update_attributes(product_attributes)
+          Shopify::AssociationSaver.save_pos_product_id(spree_product, shopify_product)
+
+          shopify_product
+        end
+
+        def export_product
+          exporter.new(spree_product: spree_product).perform
+        end
+
+        def product_attributes
+          attributor.new(spree_product).attributes
+        end
+      end
+    end
+  end
+end
---
+++ app/updaters/spree/retail/shopify/variant_updater.rb
@@ -0,0 +1,39 @@
+module Spree
+  module Retail
+    module Shopify
+      class VariantUpdater
+        def initialize(spree_variant_id:, variant_klass: Spree::Variant,
+                       variant_api: ShopifyAPI::Variant)
+
+          @spree_variant = variant_klass.find(spree_variant_id)
+          @variant_api = variant_api
+        end
+
+        def perform
+          shopify_variant = find_shopify_variant_for(spree_variant)
+          if shopify_variant.persisted?
+            shopify_variant.update_attributes(variant_attributes)
+          else
+            shopify_variant.attributes = variant_attributes
+            shopify_variant.save
+          end
+          Shopify::AssociationSaver.save_pos_variant_id(spree_variant, shopify_variant)
+
+          shopify_variant
+        end
+
+        private
+
+        attr_accessor :spree_variant, :variant_api
+
+        def find_shopify_variant_for(spree_variant)
+          variant_api.find_or_initialize_by_id(spree_variant.pos_variant_id, params: { product_id: spree_variant.product.pos_product_id })
+        end
+
+        def variant_attributes
+          Shopify::VariantAttributes.new(spree_variant).attributes
+        end
+      end
+    end
+  end
+end
