d374c747d4223169d334c82f784a24594991e294
Charles-Andre Bouffard <cabouffard@gmail.com>
2016-10-05 17:40:17 -0400

    Ensure that the hooks order controller calls the generator

--- app/controllers/spree/retail/shopify/hooks/orders_controller.rb
+++ app/controllers/spree/retail/shopify/hooks/orders_controller.rb
@@ -4,7 +4,14 @@ module Spree
       module Hooks
         class OrdersController < HooksController
           def create
+            shopify_order = ShopifyAPI::Order.find(shopify_order_id)
+            Spree::Retail::Shopify::GeneratePosOrder.new(shopify_order).process
+
             head :ok
+          end
+
+          def shopify_order_id
+            json_body['order']['id']
           end
         end
       end
