c668a06f7092d16ac8305d057310f407d3bed5f1
Charles-Andre Bouffard <cabouffard@gmail.com>
2016-10-10 09:38:45 -0400

    Add a service class that verifies the ShopifyPayload

--- app/controllers/spree/retail/shopify/hooks_controller.rb
+++ app/controllers/spree/retail/shopify/hooks_controller.rb
@@ -11,14 +11,8 @@ module Spree
         def verify_request_authenticity
           data = request.body.read
 
-          head :unauthorized unless request_authentified?(data, Spree::Retail::Config.shopify_webhook_shared_secret)
-        end
-
-        def request_authentified?(data, secret)
-          hmac_header = env['HTTP_X_SHOPIFY_HMAC_SHA256']
-          digest = OpenSSL::Digest::Digest.new('sha256')
-          calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, secret, data)).strip
-          calculated_hmac == hmac_header
+          payload_validator = Spree::Retail::Shopify::ShopifyPayload.new(data, env)
+          head :unauthorized unless payload_validator.valid?
         end
 
         def json_body
