41eb0d605b49263e4c9f0e3d6fccd1b435421cc4
Charles-Andre Bouffard <cabouffard@gmail.com>
2016-10-13 13:29:43 -0400

    Add class that will import refund from Shopify orders

--- app/models/spree/retail/shopify/generate_refund_order.rb
+++ app/models/spree/retail/shopify/generate_refund_order.rb
@@ -7,50 +7,77 @@ module Spree
         end
 
         def process
-          spree_refund = create_refund
-          spree_refund.payment = find_payment(shopify_refund)
-          spree_refund.transaction_id = shopify_refund.transactions.first.id
-          spree_refund.created_at = shopify_refund.created_at
-          spree_refund.amount = refund_amount_for(shopify_refund)
-          spree_refund.refund_reason_id = refund_reason.id
-          spree_refund.save!
+          order = find_order(shopify_refund)
 
-          shopify_refund
-        rescue => e
-          Rails.logger.error("shopify_refund id ##{shopify_refund.try(:id)}: #{e}")
+          return_items = []
+          shopify_refund.refund_line_items.each do |rli|
+            inventory_unit = find_inventory_by_shopify_variant_id(order, rli.line_item.variant_id)
+            return_items << create_return_item(inventory_unit)
+          end
+
+          create_return_authorization(order, return_items)
+          customer_return = create_customer_return(return_items)
+          create_reimbursement(customer_return)
         end
 
         private
 
-        attr_reader :shopify_refund
-
-        def create_refund
-          Spree::Refund.new
+        def find_inventory_by_shopify_variant_id(order, shopify_variant_id)
+          order.shipments.first.inventory_units.find { |unit| unit.variant.pos_variant_id.to_i == shopify_variant_id.to_i }
         end
+
+        def create_return_item(inventory_unit)
+          return_item = Spree::ReturnItem.create(
+            inventory_unit: inventory_unit,
+            preferred_reimbursement_type: reimbursement_type
+          )
+          return_item.accept!
+          return_item
+        end
+
+        def create_return_authorization(order, return_items)
+          Spree::ReturnAuthorization.create(
+            order: order,
+            stock_location: stock_location_to_refund,
+            return_reason_id: return_reason.id,
+            memo: "Automated refund made by Shopify",
+            return_items: return_items
+          )
+        end
+
+        def create_customer_return(return_items)
+          Spree::CustomerReturn.create(
+            stock_location: stock_location_to_refund,
+            return_items: return_items
+          )
+        end
+
+        def create_reimbursement(customer_return)
+          reimbursement = Spree::Reimbursement.build_from_customer_return(customer_return)
+          reimbursement.save
+          reimbursement.perform!
+        end
+
+        attr_reader :shopify_refund
 
         def find_order(shopify_refund)
           Spree::Order.find_by(pos_order_id: order_id_for(shopify_refund))
-        end
-
-        def find_payment(shopify_refund)
-          # NOTE: There will always only be one payment for a Shopify order
-          spree_order = find_order(shopify_refund)
-          spree_order.payments.first
         end
 
         def order_id_for(shopify_refund)
           shopify_refund.prefix_options[:order_id]
         end
 
-        def refund_amount_for(shopify_refund)
-          # Get all the amount of all the transaction,
-          # Convert from string to float,
-          # Sum them up.
-          shopify_refund.transactions.map(&:amount).map(&:to_f).inject(:+)
+        def return_reason
+          Spree::ReturnReason.first
         end
 
-        def refund_reason
-          Spree::RefundReason.first
+        def stock_location_to_refund
+          Spree::StockLocation.first
+        end
+
+        def reimbursement_type
+          Spree::ReimbursementType.first
         end
       end
     end
--- spec/models/spree/retail/shopify/generate_refund_order_spec.rb
+++ spec/models/spree/retail/shopify/generate_refund_order_spec.rb
@@ -8,26 +8,32 @@ Spree.describe Spree::Retail::Shopify::GenerateRefundOrder, type: :model do
   let!(:stock_location) { create :stock_location, admin_name: 'POPUP' }
   let!(:payment_method) { create :retail_payment_method }
   let!(:refund_reason) { create :refund_reason }
+  let!(:return_reason) { create :return_reason }
+  let!(:reimbursement_type) { create :reimbursement_type }
   let!(:source) { create :credit_card, name: 'POS' }
-  let(:variant) { create :variant }
+  let!(:order) { create(:completed_order_with_totals, pos_order_id: '3937615427') }
 
-  let!(:order)  { create :order, pos_order_id: '3937615427' }
-
-  let!(:payment) do
-    payment = order.payments.create(payment_method: payment_method)
-    payment.update(amount: 13.07)
-  end
-
-  let!(:response_mock) { mock_request('refunds', 'orders/3937615427/refunds/149756227', 'json') }
+  let!(:refund_response_mock) { mock_request('refunds', 'orders/3937615427/refunds/149756227', 'json') }
   let(:refund_response) { ShopifyAPI::Refund.find('149756227', params: { order_id: '3937615427' }) }
 
-  subject { described_class.new(refund_response) }
+  before do
+    order.shipments.first.inventory_units.first.variant.update(pos_variant_id: 29637399811)
+    allow_any_instance_of(Spree::Reimbursement).to receive(:perform!).and_return(true)
+  end
 
   describe '#process' do
     subject { described_class.new(refund_response).process }
 
-    it 'successfully creates a solidus refund' do
+    it 'successfully creates a spree refund' do
       expect{ subject }.to change(Spree::Refund, :count).by 1
+    end
+
+    it 'successfully creates a spree reimbursement' do
+      expect{ subject }.to change(Spree::Reimbursement, :count).by 1
+    end
+
+    it 'successfully creates a spree customer return' do
+      expect{ subject }.to change(Spree::CustomerReturn, :count).by 1
     end
   end
 end
