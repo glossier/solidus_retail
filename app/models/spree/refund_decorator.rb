8907f9cfd03d8a90968a551add7eb98eea142b2b
Charles-Andre Bouffard <cabouffard@gmail.com>
2016-10-14 17:03:58 -0400

    Try to set the order to returned
    Since we are skipping a few state for the customer_return and
    return_authorization, we have to go back the tree and try to refund all
    the inventory_units of a refund and then try to set the order state to
    return.

--- app/models/spree/refund_decorator.rb
+++ app/models/spree/refund_decorator.rb
@@ -4,12 +4,27 @@ module Spree::Retail
     delegate :pos_refunded, to: :reimbursement
 
     def perform!
-      if pos_refunded?
+      if pos_refunded
+        return_all_inventory_unit!(reimbursement)
+        try_set_order_to_return!(reimbursement)
         update_order
+
         return true
       else
         super
       end
+    end
+
+    private
+
+    def return_all_inventory_unit!(reimbursement)
+      reimbursement.return_items.each do |return_item|
+        return_item.inventory_unit.return!
+      end
+    end
+
+    def try_set_order_to_return!(reimbursement)
+      reimbursement.order.return!
     end
   end
 end
