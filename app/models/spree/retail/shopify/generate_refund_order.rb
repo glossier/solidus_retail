0b45996509503998de344487b5d3a3d880138918
Charles-Andre Bouffard <cabouffard@gmail.com>
2016-10-17 14:48:26 -0400

    Reduce local assignments for the refunder class
    As suggested in the CR
    
    https://github.com/glossier/solidus_retail/pull/49#discussion_r83675159

--- app/models/spree/retail/shopify/generate_refund_order.rb
+++ app/models/spree/retail/shopify/generate_refund_order.rb
@@ -4,23 +4,30 @@ module Spree
       class GenerateRefundOrder
         def initialize(shopify_refund)
           @shopify_refund = shopify_refund
+          @order = find_order(shopify_refund)
         end
 
         def process
-          order = find_order(shopify_refund)
-
-          return_items = []
-          shopify_refund.refund_line_items.each do |rli|
-            inventory_unit = find_inventory_by_shopify_variant_id(order, rli.line_item.variant_id)
-            return_items << create_return_item(inventory_unit)
-          end
-
           create_return_authorization(order, return_items)
           customer_return = create_customer_return(return_items)
           create_reimbursement(customer_return)
         end
 
         private
+
+        attr_reader :shopify_refund, :order
+
+        def return_items
+          @return_items ||= begin
+            return_items = []
+            shopify_refund.refund_line_items.each do |rli|
+              inventory_unit = find_inventory_by_shopify_variant_id(order, rli.line_item.variant_id)
+              return_items << create_return_item(inventory_unit)
+            end
+
+            return_items
+          end
+        end
 
         def find_inventory_by_shopify_variant_id(order, shopify_variant_id)
           order.shipments.first.inventory_units.find { |unit| unit.variant.pos_variant_id.to_i == shopify_variant_id.to_i }
@@ -58,8 +65,6 @@ module Spree
           reimbursement.pos_refunded!
           reimbursement.perform!
         end
-
-        attr_reader :shopify_refund
 
         def find_order(shopify_refund)
           Spree::Order.find_by(pos_order_id: order_id_for(shopify_refund))
