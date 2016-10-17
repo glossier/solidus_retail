e52fc621cbd13830e315c81eb3f89c13121e53a4
Charles-Andre Bouffard <cabouffard@gmail.com>
2016-10-17 14:41:05 -0400

    Call the method set to order to return from the reimbursement
    This way Demeter won't be mad at us.

--- app/models/spree/refund_decorator.rb
+++ app/models/spree/refund_decorator.rb
@@ -1,13 +1,14 @@
 module Spree::Retail
   module RefundDecorator
     delegate :pos_order_id, to: :payment
-    delegate :pos_refunded, :pos_refunded?, to: :reimbursement
+    delegate :pos_refunded, :pos_refunded?,
+             :try_set_order_to_return!, to: :reimbursement
 
     def perform!
       return super if reimbursement.nil? || !pos_refunded?
 
       return_all_inventory_unit!(reimbursement)
-      try_set_order_to_return!(reimbursement)
+      try_set_order_to_return!
       update_order
 
       true
@@ -19,10 +20,6 @@ module Spree::Retail
       reimbursement.return_items.each do |return_item|
         return_item.inventory_unit.return!
       end
-    end
-
-    def try_set_order_to_return!(reimbursement)
-      reimbursement.order.return!
     end
   end
 end
--- app/models/spree/reimbursement_decorator.rb
+++ app/models/spree/reimbursement_decorator.rb
@@ -3,6 +3,10 @@ module Spree::Retail
     def pos_refunded!
       update_attribute(:pos_refunded, true)
     end
+
+    def try_set_order_to_return!
+      order.return!
+    end
   end
 end
 
