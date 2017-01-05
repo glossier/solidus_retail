0d42d97d85252ca9cdab209a5eb7d4a8e8a9501b
emmagilhuly <emmagilhuly@me.com>
2017-01-05 14:28:01 -0500

    Specs are green for payment_spec and refund_spec

--- spec/models/spree/refund_spec.rb
+++ spec/models/spree/refund_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 module Spree
   RSpec.describe Refund do
-    let(:order) { build_stubbed(:order, pos_order_id: '8675309') }
-    let(:payment) { build_stubbed(:payment, order: order) }
-    let(:refund) { build_stubbed(:refund, payment: payment) }
+    let(:order) { create :order, pos_order_id: '8675309' }
+    let(:payment) { create :payment, order: order }
+    subject(:refund) { build_stubbed(:refund, payment: payment) }
 
     it 'knows its retail order ID' do
       expect(refund.pos_order_id).to eq '8675309'
